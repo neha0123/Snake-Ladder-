7f8e876edfe3dcddd4ad05009fe915b52e060218
Neha  Garg <50827585+neha0123@users.noreply.github.com>
2025-07-05 19:23:03 +0530

    Add files via upload

---
+++ Ladder.java
@@ -0,0 +1,17 @@
+public class Ladder {
+  private   int start;
+  private   int end;
+  Ladder(int start, int end) {
+      this.start = start;
+      this.end = end;
+  }
+
+    public int getEnd() {
+        return end;
+    }
+    public int getStart() {
+      return start;
+    }
+
+
+}
---
+++ Snake.java
@@ -0,0 +1,16 @@
+public class Snake {
+    private   int head;
+    private   int tail;
+    Snake(int head, int tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+    public int getHead() {
+        return head;
+    }
+
+}
